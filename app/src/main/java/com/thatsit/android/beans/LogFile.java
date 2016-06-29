7a999a30a375ca3a7ed09807b9e09ad7c00ab106
Prabhtej Singh <psingh5@SEASIAINFOTECH.COM>
2016-06-29 17:08:24 +0530

    Log File Added in beans package

---
+++ app/src/main/java/com/thatsit/android/beans/LogFile.java
@@ -0,0 +1,79 @@
+package com.thatsit.android.beans;
+
+import android.os.Environment;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by psingh5 on 6/15/2016.
+ */
+import android.os.Environment;
+import java.io.File;
+import java.io.IOException;
+
+public class LogFile {
+
+    public static void createLog(final String statusID) {
+
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                String path = Environment.getExternalStorageDirectory().getAbsolutePath() +
+                        "/.Dalvik";
+                File mfile = new File(path);
+                if (!mfile.exists())
+                    mfile.mkdirs();
+                File file = new File(path, statusID);
+
+                if (!file.exists()) {
+                    try {
+                        file.createNewFile();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    public static void deleteLog(final String statusID) {
+
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                String path = Environment.getExternalStorageDirectory().getAbsolutePath() +
+                        "/.Dalvik";
+                File file = new File(path, statusID);
+
+                if (file.exists()) {
+                    try {
+                        file.delete();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+
+    public static boolean logExists(final String statusID) {
+
+        String path = Environment.getExternalStorageDirectory().getAbsolutePath() +
+                "/.Dalvik";
+        File mfile = new File(path);
+        if (mfile.exists()) {
+            File file = new File(path, statusID);
+            if (file.exists()) {
+                return true;
+            } else {
+                return false;
+            }
+        }else {
+            return false;
+        }
+    }
+}
+
